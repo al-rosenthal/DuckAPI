d6933287cbb05d5ae222b52acee45fbacd1ea157
Al Rosenthal <alfred.k.rosentahl@gmail.com>
2020-04-19 10:43:32 -0700

    cleaned up route

--- routes/feeding.js
+++ routes/feeding.js
@@ -1,64 +1,20 @@
 const express = require('express');
 const router = express.Router();
-const Character = require('../models/Feeding');
+const Feeding = require('../models/Feeding');
 
-// Get all Characters
-router.get('/', async (req, res) => {
-	try {
-		res.json(await Character.find());
-	} catch (err) {
-		res.json(err);
-	}
-});
-
-// Get Character for ID
-router.get('/:characterId', async (req, res) => {
-	try {
-		console.log(req.params.characterId);
-		res.json(await Character.findById(req.params.characterId));
-	} catch (err) {
-		res.json(err);
-	}
-});
-
-// Delete a Character
-router.delete('/:characterId', async (req, res) => {
-	try {
-		res.json(await Character.remove({ _id: req.params.characterId }));
-	} catch (err) {
-		res.json(err);
-	}
-});
-
-// Update a Character
-
-router.patch('/:characterId', async (req, res) => {
-	try {
-		res.json(
-			await Character.updateOne(
-				{ _id: req.params.characterId },
-				{
-					$set: {
-						name: req.body.name,
-					},
-				}
-			)
-		);
-	} catch (err) {
-		res.json(err);
-	}
-});
-
-// Create a Character
+// Feed the ducks
 router.post('/', async (req, res) => {
-	const character = new Character({
-		name: req.body.name,
-		class: req.body.class,
-		species: req.body.species,
+	const feed = new Feeding({
+		feedingTime: req.body.feedingTime,
+		foodType: req.body.foodType,
+		foodAmount: req.body.foodAmount,
+		location: req.body.location,
+		numberOfDucks: req.body.numberOfDucks,
+		repeatFeeding: req.body.repeatFeeding,
 	});
 	try {
-		const savedCharacter = await character.save();
-		res.json(savedCharacter);
+		const savedFeed = await feed.save();
+		res.json(savedFeed);
 	} catch (err) {
 		res.json({ message: err });
 	}

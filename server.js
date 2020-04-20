7dae6ea464dbf2b54c6da918375b36e7d2b128ca
Al Rosenthal <alfred.k.rosentahl@gmail.com>
2020-04-20 15:02:03 -0700

    added port check

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 require('dotenv/config');
 
 const app = express();
-const port = 8080;
+const port = process.env.PORT || 3000;
 
 const feedingRoutes = require('./routes/feeding');
 
